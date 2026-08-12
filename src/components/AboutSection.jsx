2bf2f295ba65484b108288668a56191860a15937
Adarsh <howsillyistoosilly@gmail.com>
2026-08-12 11:25:09 +0530

    update AboutMe

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -19,7 +19,7 @@ function AboutSectionComponent() {
           </div>
         </div>
         <div className="about-right">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br /><br />Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p> hihiiii im adarsh, 20 now (and as long as father time allows) and this is my website. I love making games and learning cool stuff <br> I love listening to music and try to make cool stuff happen</p>
         </div>
       </div>
     </div>
